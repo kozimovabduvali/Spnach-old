81167d3151d5cfb2a59adb703451cd121c65c808
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-07-16 22:34:57 +0500

    push

--- src/components/Howto.jsx
+++ src/components/Howto.jsx
@@ -33,7 +33,7 @@ export default function Howto() {
                     loop
                     playsInline
                     preload="auto"
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-fill"
                     crossOrigin="anonymous"
                     loading="lazy"
                 >
